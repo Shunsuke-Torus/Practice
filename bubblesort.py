1f8a3de3dbcea2190edd05319feb0824a288a7ee
Shunsuke-Torus <37583616+Shunsuke-Torus@users.noreply.github.com>
2021-06-12 22:19:43 +0900

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,23 +1,20 @@
-#ソートを書いてみる
-
 def bubblesort(int_list):
-    for index in range (len(int_list)-1,0,-1):
-        for low in range(index):
-            if int_list[low] > int_list[low+1]:
-                tmp = int_list[low+1]
-                int_list[low+1] = int_list[low]
-                int_list[low] = tmp
+    for i in range (len(int_list)-1,0,-1):
+        for j in range(i):
+            if int_list[j] > int_list[j+1]:
+                int_list[j],int_list[j+1] = int_list[j+1],int_list[j]
+                
     print("Soreted")
     return int_list
     
 import random
 import time
 
-start_time = time.perf_counter()       #Before time
+start_time = time.perf_counter()       # 処理前の時刻
 int_list= [random.randint(0,100)for i in range(100)]
 
 
 print(bubblesort(int_list))
-elapsed_time = time.perf_counter() - start_time #After time
+elapsed_time = time.perf_counter() - start_time
 
 print("elapsed_time =",elapsed_time)
